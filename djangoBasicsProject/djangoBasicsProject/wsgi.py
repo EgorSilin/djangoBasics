df5e1f40a62df3eebef54b6e4e65f0fb0215dc19
egor_s <egorsilin@gmail.com>
2022-05-23 00:22:49 +0300

    Code style fix

--- djangoBasicsProject/djangoBasicsProject/wsgi.py
+++ djangoBasicsProject/djangoBasicsProject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBasicsProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoBasicsProject.settings")
 
 application = get_wsgi_application()
