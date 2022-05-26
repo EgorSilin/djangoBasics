c53e95410dfd79d012cec4a39cecaa1bb3c9390f
egor_s <egorsilin@gmail.com>
2022-05-26 22:19:50 +0300

    fix project structure

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""djangoBasicsProject URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="mainapp/")),
-    path("mainapp/", include("mainapp.urls")),
+    path('admin/', admin.site.urls),
 ]
