6489b079c2228ac35356b7a379959aab5f6c742c
egor_s <egorsilin@gmail.com>
2022-06-05 23:32:55 +0300

    added mark_deleted in lessons admin page

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -15,8 +15,14 @@ class LessonAdmin(admin.ModelAdmin):
     ordering = ["-course__name", "-num"]
     list_per_page = 5
     list_filter = ["course", "created", "deleted"]
+    actions = ["mark_deleted"]
 
     def get_course_name(self, obj):
         return obj.course.name
 
     get_course_name.short_description = _("Course")
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_deleted.short_description = _("Mark deleted")
