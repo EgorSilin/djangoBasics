fc84ad5116a0236bdb734bc4776fd78a7c9a2914
egor_s <egorsilin@gmail.com>
2022-06-05 23:08:57 +0300

    extend lessons view in admins

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -6,3 +6,8 @@ from mainapp import models as mainapp_models
 @admin.register(mainapp_models.News)
 class NewsAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "num", "title", "deleted"]
