39df4fc1f054fbc58fc4c3e38d1d1c25e390edb2
egor_s <egorsilin@gmail.com>
2022-06-05 23:13:23 +0300

    added course ordering in lessons admin page

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -12,6 +12,7 @@ class NewsAdmin(admin.ModelAdmin):
 @admin.register(mainapp_models.Lesson)
 class LessonAdmin(admin.ModelAdmin):
     list_display = ["id", "get_course_name", "num", "title", "deleted"]
+    ordering = ["-course__name", "-num"]
 
     def get_course_name(self, obj):
         return obj.course.name
